d6bb82c6364cc41d04be5e57d1965a833eb0a990
unknown <siegel.tucker@gmail.com>
2018-12-31 00:55:02 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ serving_port="9000"
 git pull
 
 screen -dm -S flask_server python serve.py $1
-screen -dm -S tensorflow_server tensorflow_model_server --model_base_path=/home/tuckers_krow_network/SimilarityModel/models/ --rest_api_port=9000 --model_name=$1
+# screen -dm -S tensorflow_server tensorflow_model_server --model_base_path=/home/tuckers_krow_network/SimilarityModel/models/ --rest_api_port=9000 --model_name=$1
--- serve.py
+++ serve.py
@@ -1,6 +1,6 @@
 from keras.preprocessing.sequence import pad_sequences
 from scipy.spatial.distance import cosine
-# from keras.model import load_model, Model 
+from keras.models import load_model, Model 
 from scipy import linalg, mat, dot
 import numpy as np
 import flask
@@ -21,6 +21,11 @@ stop_words = set(stopwords.words('english'))
 
 w2v = pickle.load(open("w2v.bin", "rb"))
 v2w = pickle.load(open("v2w.bin", "rb"))
+
+model = load_model("model.h5")
+word2vec = Model(inputs=model.input[0], output=model.get_layer("embedding").output)
+
+del model
 
 def clean(docs):
     docs2 = []
@@ -75,20 +80,8 @@ def create_vec(data):
     data = [subword(w, w2v) for w in data]
    
     data = pad_sequences(data, maxlen=25, value=len(w2v), padding="post")
-    # print (data[0])
-    payload = {
-        "signature_name":"serving_default",
-        "instances": [
-            {"input_words": data.tolist()}
-        ]
-    }
-    r = requests.post('http://localhost:9000/v1/models/%s:predict' % sys.argv[1], json=payload)
-
-    x = (r.json()["predictions"])
-    x = np.array(x)
-    x = sum(np.array(x[0]))
-    pred = json.loads(r.content.decode('utf-8'))
-    return x
+    pred = sum(word2vec.predict(data)[0])
+    return pred
 
 def calculate_similarity(v1, v2):
     sim = 1 - cosine(v1, v2)
