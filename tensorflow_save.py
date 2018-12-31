8758429a2e65d7c581d48878e6f7536e677671c8
unknown <siegel.tucker@gmail.com>
2018-12-30 21:27:26 -0500

    asdf

--- tensorflow_save.py
+++ tensorflow_save.py
@@ -7,6 +7,8 @@ import pickle
 import numpy as np
 from keras import backend as K
 import tensorflow as tf 
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import tag_constants, signature_constants, signature_def_utils_impl
 
 
 x = load_model("model.h5")
@@ -17,7 +19,7 @@ print ([layer.name for layer in x.layers])
 word2vec = Model(inputs=x.input[0], output=x.get_layer("embedding").output)
 del x
 
-# with K.get_session() as sess:
+with K.get_session() as sess:
 #     tf.saved_model.simple_save(
 #         sess,
 #         "models/1",
@@ -25,20 +27,20 @@ del x
 #         outputs={t.name:t for t in word2vec.outputs}
 #     )
 
-from tensorflow.python.saved_model import builder as saved_model_builder
-from tensorflow.python.saved_model import utils
-from tensorflow.python.saved_model import tag_constants, signature_constants
-from tensorflow.python.saved_model.signature_def_utils_impl import     build_signature_def, predict_signature_def
-from tensorflow.contrib.session_bundle import exporter
+# print (word2vec.input)
+    prediction_signature = tf.saved_model.signature_def_utils.predict_signature_def({"input_words": word2vec.input}, {"prediction":word2vec.output})
 
-export_path = 'models/2'
-builder = saved_model_builder.SavedModelBuilder(export_path)
+    valid_prediction_signature = tf.saved_model.signature_def_utils.is_valid_signature(prediction_signature)
+    if(valid_prediction_signature == False):
+        raise ValueError("Error: Prediction signature not valid!")
+        
+    builder = saved_model_builder.SavedModelBuilder('models/2')
+    legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+    builder.add_meta_graph_and_variables(
+        sess, [tag_constants.SERVING],
+        signature_def_map={
+            signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:prediction_signature,
+        },
+        legacy_init_op=legacy_init_op)
 
-signature = predict_signature_def(inputs={'input_words': word2vec.input},
-                                  outputs={'embedding': word2vec.output})
-
-with K.get_session() as sess:
-    builder.add_meta_graph_and_variables(sess=sess,
-                                         tags=[tag_constants.SERVING],
-                                         signature_def_map={'predict': signature})
     builder.save()
